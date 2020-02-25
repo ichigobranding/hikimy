b2bcd0f9ab670eb3295e5f426cfa87038a942f8a
ichigobranding <info@ichigo-branding.com>
2020-02-25 13:22:03 +0900

    投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,14 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
-    @id = params[:id]
   end
 
   def new
   end
 
   def create
+    @post = Post.new(content: params[:content])
+    @post.save
     redirect_to("/posts")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
   post 'posts/create', to: 'posts#create'
-  get '/', to: 'home#index'
+  get '/', to: 'posts#index'
   # get '/', to: 'home#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
