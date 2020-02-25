7f502d40e954cb1504a0888a4bcde61a0aaff4f3
ichigobranding <info@ichigo-branding.com>
2020-02-26 08:42:28 +0900

    質問投稿の編集・削除追加 レイアウトの調整 不要コード削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
 
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    # id を取得
+    @post = Post.find_by(id: params[:id])
+    # content上書き
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/#{@post.id}")
+  end
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -16,4 +29,11 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts")
   end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
+  get 'posts/:id/edit', to: 'posts#edit'
+  post 'posts/:id/update', to: 'posts#update'
+  post 'posts/:id/destroy', to: 'posts#destroy'
   get 'posts/:id', to: 'posts#show'
   post 'posts/create', to: 'posts#create'
   get '/', to: 'posts#index'
