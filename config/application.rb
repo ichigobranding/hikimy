bdaf63386f5f0ff5ec579d109d116d55195c8edd
ichigobranding <info@ichigo-branding.com>
2020-02-28 06:09:05 +0900

    日本語訳変更中

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,9 @@ module Hikimy
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+    config.i18n.default_locale = :ja
+  end
+
 end
+
