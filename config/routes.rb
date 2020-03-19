7c491f1d415f25f23346cf87ee034131525528d9
ichigobranding <info@ichigo-branding.com>
2020-03-19 12:26:52 +0900

    ユーザー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
+  get '/users/index', to: 'users#index'
   get '/users/:id/edit', to: 'users#edit'
   post '/users/:id/update', to: 'users#update'
   post '/users/:id/destroy', to: 'users#destroy'
   get '/signup', to: 'users#new'
   post  '/signup',   to: 'users#create'
   get '/users/:id', to: 'users#show'
-  # post '/users/create', to:'users#create'
+  post '/users/create', to:'users#create'
 
-  get '/users', to: 'users#index'
 
   get '/posts', to: 'posts#index'
   get '/posts/new', to: 'posts#new'
